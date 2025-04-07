968a7172325530f896ec188e1963a356d7301651
Sadakat Hossain <sadakat.sh1@gmail.com>
2025-04-07 23:49:41 +0700

    Succesfully connect with mongoDb and loaded Data

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ app.use(cors());
 app.use(express.json());
 
 
-// connect with MongoDB
+
+//--------------------------- Start Of MongoDB Connection Code -------------------------------//
 
 // const uri = "mongodb+srv://sadakathossain11:<db_password>@cluster1.csyfged.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
-const uri = `mongodb+srv://sadakathossain11:${process.env.DB_PASS}@cluster1.csyfged.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster1.csyfged.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`;
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -28,29 +30,37 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("TaskManagement").command({ ping: 1 });
-        const dataCollection = client.db("TaskManagement").collection('AllTask')
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
-        // Get operation
-        app.get('/', async (req, res) => {
-            res.send('Server is error running.')
+        //--------------------------- Start Database Code -------------------------------//
+
+        const database = client.db('TaskManagement').collection('AllTask')
+
+        app.get("/allTask", async (req, res) => {
+            const result = await database.find().toArray();
+            res.send(result);
         });
 
-        // Load data from MongoDB
-        app.get('/allTask', async (req, res) => {
-            const result = await dataCollection.find().toArray();
-            res.send(result)
-        })
+        //--------------------------- Finished Database Code -------------------------------//
 
 
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
+
+//--------------------------- End Of MongoDB Connection Code -------------------------------//
+
+
+// get Operation
+app.get("/", async (req, res) => {
+    res.send("Error server is running")
+});
 
 
 app.listen(port, () => {
